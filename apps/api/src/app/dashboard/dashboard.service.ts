408269204b58a8e3b90a6fbab6137b34fc0363c5
Subhojit Ghosh <subhojitghosh064@gmail.com>
2025-03-21 21:07:03 +0530

    ✨ feat: Implement dashboard statistics endpoint and integrate with Dashboard component

--- apps/api/src/app/dashboard/dashboard.service.ts
+++ apps/api/src/app/dashboard/dashboard.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DRIZZLE, DrizzleDB, studentsTable } from '@school-console/drizzle';
+import { count, eq } from 'drizzle-orm';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+  constructor(@Inject(DRIZZLE) private db: DrizzleDB) {}
+
+  async stats() {
+    const enrolledStudentsQuery = this.db
+      .select({ count: count() })
+      .from(studentsTable)
+      .where(eq(studentsTable.isEnrolled, true))
+      .then((res) => res[0].count);
+
+    const newRegistrationsQuery = this.db
+      .select({ count: count() })
+      .from(studentsTable)
+      .where(eq(studentsTable.isEnrolled, false))
+      .then((res) => res[0].count);
+
+    const [enrolledStudents, newRegistrations] = await Promise.all([
+      enrolledStudentsQuery,
+      newRegistrationsQuery,
+    ]);
+
+    return {
+      enrolledStudents,
+      newRegistrations,
+    };
+  }
+}
