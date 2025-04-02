4166712e6e3e9efb40095a975f9688a538660849
Subhojit Ghosh <subhojitghosh064@gmail.com>
2025-04-02 23:21:40 +0530

    ✨ FEATURE: Refactor Layout component to use Container for Outlet

--- apps/web/src/app/layout/Layout.tsx
+++ apps/web/src/app/layout/Layout.tsx
@@ -1,31 +1,16 @@
-import { AppShell, useMantineTheme } from '@mantine/core';
+import { AppShell, Container } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
 
-const gradients = {
-  // 1: 'linear-gradient(135deg, #3F51B5, #2196F3)',
-  // 2: 'linear-gradient(135deg, #00BCD4, #3F51B5)',
-  3: 'linear-gradient(135deg, #1E3C72, #2A5298)',
-  // 4: 'linear-gradient(135deg, #283E51, #4B79A1)',
-  // 5: 'linear-gradient(135deg, #3F51B5, #2196F3);',
-};
-
 function Layout() {
-  const theme = useMantineTheme();
-
   return (
     <AppShell
-      // header={{ height: 60 }}
       navbar={{
         width: 220,
         breakpoint: 'sm',
       }}
       padding="md"
     >
-      {/* <AppShell.Header bg="blue">
-        <Header />
-      </AppShell.Header> */}
-
       <AppShell.Navbar
         style={{ background: 'linear-gradient(135deg, #1E3C72, #2A5298)' }}
       >
@@ -33,7 +18,9 @@ function Layout() {
       </AppShell.Navbar>
 
       <AppShell.Main style={{ background: '#F4F6F8' }}>
-        <Outlet />
+        <Container size="xl" p={0}>
+          <Outlet />
+        </Container>
       </AppShell.Main>
     </AppShell>
   );
